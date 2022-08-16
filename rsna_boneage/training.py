c5f286c16f651070083bb01ab71775f9919d74b9
marvinweber <marvin@marvinweber.net>
2022-08-16 18:50:26 +0200

    CLI usable trainings script, configurable via csv

--- rsna_boneage/training.py
+++ rsna_boneage/training.py
@@ -1,94 +1,83 @@
+import argparse
+import os
+from datetime import datetime
+
+import pandas as pd
 from pytorch_lightning import Trainer
 from pytorch_lightning import loggers as pl_loggers
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint, StochasticWeightAveraging
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from torchsummary import summary
-from torchvision import transforms
 
-from torchvision.models import resnet18, resnet34, inception_v3
+from util.model_provider import get_model_and_datamodule
 
-from boneage.rsna_bone_net import resnet18 as resnet18_boneage, resnet34 as resnet34_boneage
-from boneage.rsna_bone_litmodel import RSNABoneageLitModel
-from boneage.rsna_bone_dataloading import RSNABoneageDataModule
-
-
-def main():
-    rsna_net = get_inception()
-
-    transform = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(30),
-        transforms.ColorJitter(brightness=0.5)
-    ])
-    rsna_boneage_datamodule = RSNABoneageDataModule(
-        annotation_file_train='./images/with_preprocessed_train_annotations.csv',
-        annotation_file_val='./images/with_preprocessed_val_annotations.csv',
-        annotation_file_test='./images/with_preprocessed_test_annotations.csv',
-        batch_size=8,
-        transform=transform,
-        target_dimensions=(299, 299),
-        rescale_boneage=True,
-        rebalance_classes=True)
-    model = RSNABoneageLitModel(net=rsna_net)
-
-    tb_logger = pl_loggers.TensorBoardLogger(save_dir=".")
-    csv_logger = pl_loggers.CSVLogger(save_dir=".")
-
-    checkpoint_callback = ModelCheckpoint(
-        monitor='val_loss', save_top_k=-1, filename='{epoch}-{val_loss:2f}')
-    early_stopping_callback = EarlyStopping(
-        monitor='val_loss', mode='min', patience=10)
-    swa_callback = StochasticWeightAveraging(swa_lrs=1e-2)
-    callbacks = [checkpoint_callback, early_stopping_callback]
-
-    trainer = Trainer(accelerator='gpu', max_epochs=100, log_every_n_steps=50,
-                      logger=[csv_logger, tb_logger], callbacks=callbacks)
-    trainer.fit(model, datamodule=rsna_boneage_datamodule)
+parser = argparse.ArgumentParser(description='Training of RSNA Boneage Models')
+parser.add_argument('model_train_config', metavar='MODEL_TRAIN_CONFIG', type=str,
+                    help='Model Configuration for Training (csv file).')
+parser.add_argument('--max-epochs', metavar='MAX_EPOCHS', type=int, default=100, required=False,
+                    help='Maximum Epochs to train.')
+parser.add_argument('--early-stopping-patience', metavar='EARLY_STOPPING_PATIENCE', type=int,
+                    default=10, required=False, help='Patience for EarlyStopping Callback.')
 
 
-def get_resnet_18():
-    resnet_pretrained = resnet18(pretrained=True, progress=True)
-    resnet_pretrained_state_dict = resnet_pretrained.state_dict()
+def train_all(model_train_config_filepath: str, max_epochs: int, early_stopping_patience: int):
+    save_dir = 'train_logs'
+    version = datetime.now().strftime('%Y-%m-%d-%H-%M')
+    train_configs = pd.read_csv(model_train_config_filepath, index_col='model_id')
+    train_log = train_configs.copy(True)
+    train_log_file = os.path.join(save_dir, f'train_log_{version}.csv')
 
-    resnet = resnet18_boneage(pretrained=False, progress=True, num_classes=1)
-    resnet_state_dict = resnet.state_dict()
+    print(f'Training Started. Max Epochs: {max_epochs}, Early Stopping '
+          f'Patience: {early_stopping_patience}.')
 
-    for key in resnet_state_dict.keys():
-        if key in resnet_pretrained_state_dict and 'fc' not in key:
-            resnet_state_dict[key] = resnet_pretrained_state_dict[key]
-    resnet.load_state_dict(resnet_state_dict)
+    for train_config in train_configs.itertuples():
+        print('\n\n\n######### NEXT MODEL ###############')
 
-    return resnet
+        img_input_dimensions=(train_config.img_input_width, train_config.img_input_height)
+        model_log_name = (f'{train_config.name}_{train_config.img_input_width}x'
+                          f'{train_config.img_input_height}')
+        if train_config.with_gender_input:
+            model_log_name = f'{model_log_name}_with_gender_input'
+        
+        print('Model:', model_log_name)
+        log_dir = os.path.join(save_dir, model_log_name, version)
+        checkpoint_dir = os.path.join(log_dir, 'checkpoints')
+        os.makedirs(checkpoint_dir, exist_ok=True)
 
+        train_log.at[train_config.Index, 'max_epochs'] = max_epochs
+        train_log.at[train_config.Index, 'early_stopping_patience'] = early_stopping_patience
+        train_log.at[train_config.Index, 'log_name'] = model_log_name
+        train_log.at[train_config.Index, 'log_dir'] = log_dir
+        train_log.at[train_config.Index, 'checkpoint_dir'] = checkpoint_dir
+        train_log.to_csv(train_log_file)
 
-def get_resnet_34():
-    resnet_pretrained = resnet34(pretrained=True, progress=True)
-    resnet_pretrained_state_dict = resnet_pretrained.state_dict()
+        model, rsna_boneage_datamodule = get_model_and_datamodule(
+            train_config.name,
+            img_input_dimension=img_input_dimensions,
+            with_gender_input=train_config.with_gender_input)
 
-    resnet = resnet34_boneage(pretrained=False, progress=True, num_classes=1)
-    resnet_state_dict = resnet.state_dict()
+        tb_logger = pl_loggers.TensorBoardLogger(
+            save_dir=save_dir, name=model_log_name, version=version)
+        csv_logger = pl_loggers.CSVLogger(
+            save_dir=save_dir, name=model_log_name, version=version)
 
-    for key in resnet_state_dict.keys():
-        if key in resnet_pretrained_state_dict and 'fc' not in key:
-            resnet_state_dict[key] = resnet_pretrained_state_dict[key]
-    resnet.load_state_dict(resnet_state_dict)
+        checkpoint_callback = ModelCheckpoint(
+            dirpath=checkpoint_dir, monitor='val_loss', save_top_k=-1,
+            filename='{epoch}-{val_loss:2f}')
+        early_stopping_callback = EarlyStopping(
+            monitor='val_loss', mode='min', patience=early_stopping_patience)
+        callbacks = [checkpoint_callback, early_stopping_callback]
 
-    return resnet
-
-
-def get_inception():
-    inception_pretrained = inception_v3(pretrained=True, progress=True)
-    inception_pretrained_state_dict = inception_pretrained.state_dict()
-
-    inception = inception_v3(pretrained=False, num_classes=1)
-    inception_state_dict = inception.state_dict()
-
-    for key in inception_state_dict.keys():
-        if key in inception_pretrained_state_dict and 'fc' not in key:
-            inception_state_dict[key] = inception_pretrained_state_dict[key]
-    inception.load_state_dict(inception_state_dict)
-
-    return inception
+        trainer = Trainer(accelerator='gpu', max_epochs=max_epochs, log_every_n_steps=50,
+                          logger=[csv_logger, tb_logger], callbacks=callbacks)
+        trainer.fit(model, datamodule=rsna_boneage_datamodule)
+        print('Training done!')
 
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    
+    model_train_config_filepath = args.model_train_config
+    max_epochs = args.max_epochs
+    early_stopping_patience = args.early_stopping_patience
+
+    train_all(model_train_config_filepath, max_epochs, early_stopping_patience)
