93c16a3f80e0a03cd75d1ce5e93b1592f10afdb3
marvinweber <marvin@marvinweber.net>
2022-09-29 17:00:52 +0200

    Except exceptions during training and continue
    instead of aborting everything

--- rsna_boneage/training.py
+++ rsna_boneage/training.py
@@ -35,47 +35,54 @@ def train_all(model_train_config_filepath: str, max_epochs: int, early_stopping_
           f'Patience: {early_stopping_patience}.')
 
     for train_config in train_configs.itertuples():
-        print('\n\n\n######### NEXT MODEL ###############')
+        print('######### NEXT MODEL ###############')
 
-        img_input_dimensions=(train_config.img_input_width, train_config.img_input_height)
-        model_log_name = (f'{train_config.name}_{train_config.img_input_width}x'
-                          f'{train_config.img_input_height}')
-        if train_config.with_gender_input:
-            model_log_name = f'{model_log_name}_with_gender_input'
-        
-        print('Model:', model_log_name)
-        log_dir = os.path.join(save_dir, model_log_name, version)
-        checkpoint_dir = os.path.join(log_dir, 'checkpoints')
-        os.makedirs(checkpoint_dir, exist_ok=True)
+        try:
+            img_input_dimensions=(train_config.img_input_width, train_config.img_input_height)
+            model_log_name = (f'{train_config.name}_{train_config.img_input_width}x'
+                            f'{train_config.img_input_height}')
+            if train_config.with_gender_input:
+                model_log_name = f'{model_log_name}_with_gender_input'
+            
+            print('Model:', model_log_name)
+            log_dir = os.path.join(save_dir, model_log_name, version)
+            checkpoint_dir = os.path.join(log_dir, 'checkpoints')
+            os.makedirs(checkpoint_dir, exist_ok=True)
 
-        train_log.at[train_config.Index, 'max_epochs'] = max_epochs
-        train_log.at[train_config.Index, 'early_stopping_patience'] = early_stopping_patience
-        train_log.at[train_config.Index, 'log_name'] = model_log_name
-        train_log.at[train_config.Index, 'log_dir'] = log_dir
-        train_log.at[train_config.Index, 'checkpoint_dir'] = checkpoint_dir
-        train_log.to_csv(train_log_file)
+            train_log.at[train_config.Index, 'max_epochs'] = max_epochs
+            train_log.at[train_config.Index, 'early_stopping_patience'] = early_stopping_patience
+            train_log.at[train_config.Index, 'log_name'] = model_log_name
+            train_log.at[train_config.Index, 'log_dir'] = log_dir
+            train_log.at[train_config.Index, 'checkpoint_dir'] = checkpoint_dir
+            train_log.to_csv(train_log_file)
 
-        model, rsna_boneage_datamodule = get_model_and_datamodule(
-            train_config.name,
-            img_input_dimension=img_input_dimensions,
-            with_gender_input=train_config.with_gender_input)
+            model, rsna_boneage_datamodule = get_model_and_datamodule(
+                train_config.name,
+                img_input_dimension=img_input_dimensions,
+                with_gender_input=train_config.with_gender_input)
 
-        tb_logger = pl_loggers.TensorBoardLogger(
-            save_dir=save_dir, name=model_log_name, version=version)
-        csv_logger = pl_loggers.CSVLogger(
-            save_dir=save_dir, name=model_log_name, version=version)
+            tb_logger = pl_loggers.TensorBoardLogger(
+                save_dir=save_dir, name=model_log_name, version=version)
+            csv_logger = pl_loggers.CSVLogger(
+                save_dir=save_dir, name=model_log_name, version=version)
 
-        checkpoint_callback = ModelCheckpoint(
-            dirpath=checkpoint_dir, monitor='val_loss', save_top_k=save_top_k_checkpoints,
-            save_last=True, filename='{epoch}-{val_loss:2f}')
-        early_stopping_callback = EarlyStopping(
-            monitor='val_loss', mode='min', patience=early_stopping_patience)
-        callbacks = [checkpoint_callback, early_stopping_callback]
+            checkpoint_callback = ModelCheckpoint(
+                dirpath=checkpoint_dir, monitor='val_loss', save_top_k=save_top_k_checkpoints,
+                save_last=True, filename='{epoch}-{val_loss:2f}')
+            early_stopping_callback = EarlyStopping(
+                monitor='val_loss', mode='min', patience=early_stopping_patience)
+            callbacks = [checkpoint_callback, early_stopping_callback]
 
-        trainer = Trainer(accelerator='gpu', max_epochs=max_epochs, log_every_n_steps=50,
-                          logger=[csv_logger, tb_logger], callbacks=callbacks)
-        trainer.fit(model, datamodule=rsna_boneage_datamodule)
-        print('Training done!')
+            trainer = Trainer(accelerator='gpu', max_epochs=max_epochs, log_every_n_steps=50,
+                            logger=[csv_logger, tb_logger], callbacks=callbacks)
+            trainer.fit(model, datamodule=rsna_boneage_datamodule)
+        except Exception as e:
+            print('!!!!!!!!!!!!!! EXCEPTION !!!!!!!!!!!!!!')
+            print(e)
+        else:
+            print('Training finished!')
+
+        print('\n\n\n\n')
 
 
 if __name__ == '__main__':
