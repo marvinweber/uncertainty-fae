11851ab908c1f2fddb2c9eae05bec3312ca33f6d
marvinweber <marvin@marvinweber.net>
2022-11-03 12:54:42 +0100

    Add generic slurm script and usage notes

---
+++ commands/generic_sbatch_uncertainty.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=24
+#SBATCH --mem=32GB
+#SBATCH -o logs/slurm/slurm%j.log
+#SBATCH -e logs/slurm/slurm%j.err
+#SBATCH -J uncertainty_fae_model_training
+
+echo "Using Image: ${UQ_FAE_SING_IMG}"
+echo "Mounts: ${UQ_FAE_SING_MOUNTS}"
+echo "###############################"
+
+singularity exec --nv --no-home -B "$UQ_FAE_SING_MOUNTS" "$UQ_FAE_SING_IMG" "$@"
