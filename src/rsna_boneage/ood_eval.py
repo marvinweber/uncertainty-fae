be7c9c5970ec62fb687e17df69ded6d78ea0ca2c
marvinweber <marvin@marvinweber.net>
2022-12-07 08:43:57 +0100

    Move plotting logic to base/parent class
    Preparation to integrate Clavicle CT OoD
    Evaluation.

--- src/rsna_boneage/ood_eval.py
+++ src/rsna_boneage/ood_eval.py
@@ -5,10 +5,6 @@ from typing import Callable
 
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from matplotlib.collections import PolyCollection
-from matplotlib.figure import Figure
-from matplotlib.patches import Patch
 
 from rsna_boneage.data import RSNABoneageDataModule
 from uncertainty_fae.evaluation import OutOfDomainEvaluator
@@ -18,22 +14,22 @@ from uncertainty_fae.util import EvalRunConfig, ModelProvider
 
 logger = logging.getLogger(__name__)
 
-HATCHES = ['xxx', '***', 'ooo']
-
 
 class RSNABoneAgeOutOfDomainEvaluator(OutOfDomainEvaluator):
-
     def __init__(
         self,
         data_base_dir: str,
         plot_base_dir: str,
         eval_run_cfg: EvalRunConfig,
-        age_transform: Callable[[pd.Series], pd.Series],
+        age_to_year_transform: Callable[[pd.Series], pd.Series],
     ) -> None:
-        super().__init__(data_base_dir, plot_base_dir, eval_run_cfg, age_transform)
-
-        self.ood_datasets: dict = self.eval_run_cfg.ood_datasets['rsna_boneage']
-        self.plot_dir = os.path.join(self.plot_base_dir, self.eval_run_cfg.start_time)
+        super().__init__(
+            data_base_dir,
+            plot_base_dir,
+            eval_run_cfg,
+            age_to_year_transform,
+            "rsna_boneage",
+        )
 
     @classmethod
     def get_evaluator(
@@ -41,33 +37,10 @@ class RSNABoneAgeOutOfDomainEvaluator(OutOfDomainEvaluator):
         data_base_dir: str,
         plot_base_dir: str,
         eval_run_cfg: EvalRunConfig,
-        age_transform: Callable[[pd.Series], pd.Series],
-    ) -> 'OutOfDomainEvaluator':
-        evaluator = cls(data_base_dir, plot_base_dir, eval_run_cfg, age_transform)
+        age_to_year_transform: Callable[[pd.Series], pd.Series],
+    ) -> "OutOfDomainEvaluator":
+        evaluator = cls(data_base_dir, plot_base_dir, eval_run_cfg, age_to_year_transform)
         return evaluator
-
-    def _get_pred_filepath(self, eval_cfg_name: str, ood_name: str) -> str:
-        return os.path.join(self.data_base_dir, f'{eval_cfg_name}_{ood_name}_ood_preds.csv')
-
-    def _load_pred_file(self, eval_cfg_name: str, ood_name: str) -> pd.DataFrame | None:
-        """Load Prediction DataFrame and apply Age Transforms."""
-        ood_pred_file = self._get_pred_filepath(eval_cfg_name, ood_name)
-        if not os.path.isfile(ood_pred_file):
-            logger.warning(
-                'No OoD-Pred file for %s, %s (%s)', eval_cfg_name, ood_name, ood_pred_file
-            )
-            return None
-        ood_preds = pd.read_csv(ood_pred_file)
-        for col in ['prediction', 'uncertainty']:
-            ood_preds[col] = self.age_transform(ood_preds[col])
-        return ood_preds
-
-    def ood_preds_avail(self, eval_cfg_name: str) -> bool:
-        for ood_name in self.ood_datasets.keys():
-            pred_file = self._get_pred_filepath(eval_cfg_name, ood_name)
-            if not os.path.exists(pred_file):
-                return False  # If single pred file missing: return False
-        return True
 
     def generate_predictions(
         self,
@@ -76,166 +49,39 @@ class RSNABoneAgeOutOfDomainEvaluator(OutOfDomainEvaluator):
         model_provider: ModelProvider,
     ) -> None:
         for ood_name, ood_cfg in self.ood_datasets.items():
-            logger.info('Next OOD Dataset: %s', ood_name)
+            logger.info("Next OOD Dataset: %s", ood_name)
             pred_file = self._get_pred_filepath(eval_cfg_name, ood_name)
 
             if os.path.exists(pred_file):
-                logger.info('Skipping, as prediction file already available...')
+                logger.info("Skipping, as prediction file already available...")
                 continue
 
             dm: RSNABoneageDataModule = model_provider.get_lightning_data_module(
                 None,
-                ood_cfg['annotations'],
+                ood_cfg["annotations"],
                 None,
-                img_val_base_dir=ood_cfg['base_dir'],
+                img_val_base_dir=ood_cfg["base_dir"],
                 batch_size=self.eval_run_cfg.batch_size,
                 num_workers=self.eval_run_cfg.dataloader_num_workers,
             )
-            dm.setup('validate')  # We always use the validation dataset
+            dm.setup("validate")  # We always use the validation dataset
             dm.dataset_val.annotations = dm.dataset_val.annotations[:10000]  # max ood samples
-            dm.dataset_val.annotations['boneage'] = 0  # we don't need the target to make sense
+            dm.dataset_val.annotations["boneage"] = 0  # we don't need the target to make sense
             # use random sex per sample
-            dm.dataset_val.annotations['male'] = np.random.randint(0, 2, size=len(dm.dataset_val))
+            dm.dataset_val.annotations["male"] = np.random.randint(0, 2, size=len(dm.dataset_val))
 
             results = model.evaluate_dataset(dm.val_dataloader())
             score, predictions, targets, errors, uncertainties, metrics = results
 
             # Prediction and Uncertainty Stats
             os.makedirs(self.data_base_dir, exist_ok=True)
-            with open(pred_file, 'w') as file:
+            with open(pred_file, "w") as file:
                 writer = csv.writer(file)
-                writer.writerow(['index', 'prediction', 'uncertainty'])
+                writer.writerow(["index", "prediction", "uncertainty"])
                 writer.writerows(
                     zip(range(len(predictions)), predictions.tolist(), uncertainties.tolist())
                 )
 
     def generate_plots(self, eval_runs_data: dict[str, EvalRunData]) -> None:
-        self._generate_uq_comparison_plot(eval_runs_data)
+        self._generate_uq_comparison_plot(eval_runs_data, orig_data_label="RSNA Bone Age")
         self._generate_prediction_comparison_plot(eval_runs_data)
-
-    def _generate_uq_comparison_plot(self, eval_runs_data: dict[str, EvalRunData]) -> None:
-        violin_positions = []
-        violin_labels = []
-        violin_datas = []
-        violin_colors = []
-        violin_hatches = []
-        violin_edge_color = []
-        legend_elements = [
-            Patch(facecolor='white', edgecolor='black', label='RSNA Bone Age'),
-        ]
-
-        v_pos = 0
-        for eval_cfg_name, eval_run_data in eval_runs_data.items():
-            v_pos += 1
-            uq_name = eval_run_data['display_name']
-            uq_preds = eval_run_data['prediction_log']
-            color = eval_run_data['color']
-
-            avail_hatches = HATCHES.copy()
-
-            # Base Entry: UQ with "normal" dataset
-            violin_positions.append(v_pos)
-            violin_labels.append(uq_name)
-            violin_datas.append(uq_preds['uncertainty'].tolist())
-            violin_colors.append(color)
-            violin_hatches.append(None)  # no hatch for baseline dataset
-            violin_edge_color.append('black')
-
-            for ood_name, ood_cfg in self.ood_datasets.items():
-                ood_preds = self._load_pred_file(eval_cfg_name, ood_name)
-                if ood_preds is None:
-                    continue
-
-                v_pos += 1
-                violin_positions.append(v_pos)
-                # violin_labels.append(f'{uq_name} - {ood_cfg["name"]}')
-                violin_labels.append('')
-                violin_datas.append(ood_preds['uncertainty'].tolist())
-                violin_colors.append(color)
-                hatch = avail_hatches.pop(0)
-                violin_hatches.append(hatch)
-                violin_edge_color.append('white')
-
-                if len(legend_elements) < len(self.ood_datasets.items()) + 1:
-                    patch = Patch(
-                        facecolor='white', edgecolor='grey', hatch=hatch, label=ood_cfg['name'])
-                    legend_elements.append(patch)
-            v_pos += 1
-
-        fig, ax = self._get_fig()
-        vplots = ax.violinplot(violin_datas, violin_positions, showmeans=True, widths=1)
-        v_patch: PolyCollection
-        for v_patch, color, hatch in zip(vplots['bodies'], violin_colors, violin_hatches):
-            v_patch.set_facecolor(color)
-            if hatch:
-                v_patch.set_hatch(hatch)
-                v_patch.set_edgecolor(color)
-        for partname in ('cbars', 'cmins', 'cmaxes', 'cmeans'):
-            vplots[partname].set_edgecolor('black')
-            vplots[partname].set_linewidth(1)
-        ax.set_xticks(violin_positions)
-        ax.set_xticklabels(violin_labels, rotation=15)
-        ax.set_ylabel('Uncertainty')
-        ax.legend(handles=legend_elements, handleheight=3, handlelength=4, loc='upper left')
-        self._save_fig(fig, 'uq_comparison')
-
-    def _generate_prediction_comparison_plot(self, eval_runs_data: dict[str, EvalRunData]) -> None:
-        """Violin-Plot Comparison of the Predictions for the OOD Datasets by the UQ-Models."""
-        violin_positions = []
-        violin_labels = []
-        violin_datas = []
-        violin_colors = []
-        violin_hatches = []
-        violin_edge_color = []
-        legend_elements = []
-
-        v_pos = 0
-        for eval_cfg_name, eval_run_data in eval_runs_data.items():
-            uq_name = eval_run_data['display_name']
-            color = eval_run_data['color']
-
-            avail_hatches = HATCHES.copy()
-
-            for ood_name, ood_cfg in self.ood_datasets.items():
-                ood_preds = self._load_pred_file(eval_cfg_name, ood_name)
-                if ood_preds is None:
-                    continue
-
-                v_pos += 1
-                violin_positions.append(v_pos)
-                violin_labels.append(uq_name)
-                violin_datas.append(ood_preds['prediction'].tolist())
-                violin_colors.append(color)
-                hatch = avail_hatches.pop(0)
-                violin_hatches.append(hatch)
-                violin_edge_color.append('white')
-
-                if len(legend_elements) < len(self.ood_datasets.items()):
-                    patch = Patch(
-                        facecolor='white', edgecolor='grey', hatch=hatch, label=ood_cfg['name'])
-                    legend_elements.append(patch)
-            v_pos += 1
-
-        fig, ax = self._get_fig()
-        vplots = ax.violinplot(violin_datas, violin_positions, showmeans=True, widths=1)
-        v_patch: PolyCollection
-        for v_patch, color, hatch in zip(vplots['bodies'], violin_colors, violin_hatches):
-            v_patch.set_facecolor(color)
-            if hatch:
-                v_patch.set_hatch(hatch)
-                v_patch.set_edgecolor(color)
-        for partname in ('cbars', 'cmins', 'cmaxes', 'cmeans'):
-            vplots[partname].set_edgecolor('black')
-            vplots[partname].set_linewidth(1)
-        ax.set_xticks(violin_positions)
-        ax.set_xticklabels(violin_labels, rotation=15)
-        ax.set_ylabel('Prediction (Age in Years)')
-        ax.legend(handles=legend_elements, handleheight=3, handlelength=4, loc='upper right')
-        self._save_fig(fig, 'prediction_comparison')
-
-    def _get_fig(self):
-        return plt.subplots(figsize=(10, 7), dpi=250)
-
-    def _save_fig(self, fig: Figure, name: str) -> None:
-        os.makedirs(self.plot_dir, exist_ok=True)
-        fig.savefig(os.path.join(self.plot_dir, f'{name}.png'))
--- src/uncertainty_fae/evaluation/ood.py
+++ src/uncertainty_fae/evaluation/ood.py
@@ -1,26 +1,41 @@
+import logging
+import os
 from abc import ABC, abstractmethod
 from typing import Callable
 
-from pandas import Series
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.collections import PolyCollection
+from matplotlib.figure import Figure
+from matplotlib.patches import Patch
 
 from uncertainty_fae.evaluation.util import EvalRunData
 from uncertainty_fae.model import UncertaintyAwareModel
 from uncertainty_fae.util import EvalRunConfig, ModelProvider
 
+logger = logging.getLogger(__file__)
+
+HATCHES = ["xxx", "***", "ooo"]
+
 
 class OutOfDomainEvaluator(ABC):
-
     def __init__(
         self,
         data_base_dir: str,
         plot_base_dir: str,
         eval_run_cfg: EvalRunConfig,
-        age_transform: Callable[[Series], Series],
+        age_to_year_transform: Callable[[pd.Series], pd.Series],
+        ood_dataset_cfg_key: str,
     ) -> None:
         self.data_base_dir = data_base_dir
         self.plot_base_dir = plot_base_dir
         self.eval_run_cfg = eval_run_cfg
-        self.age_transform = age_transform
+        self.age_to_year_transform = age_to_year_transform
+
+        self.ood_datasets: dict[str, dict[str, str]] = self.eval_run_cfg.ood_datasets[
+            ood_dataset_cfg_key
+        ]
+        self.plot_dir = os.path.join(self.plot_base_dir, self.eval_run_cfg.start_time)
 
     @classmethod
     @abstractmethod
@@ -29,8 +44,8 @@ class OutOfDomainEvaluator(ABC):
         data_base_dir: str,
         plot_base_dir: str,
         eval_run_cfg: EvalRunConfig,
-        age_transform: Callable[[Series], Series],
-    ) -> 'OutOfDomainEvaluator':
+        age_to_year_transform: Callable[[pd.Series], pd.Series],
+    ) -> "OutOfDomainEvaluator":
         """
         Create new instance of an `OutofDomainEvaluator`.
 
@@ -38,16 +53,19 @@ class OutOfDomainEvaluator(ABC):
             data_base_dir: The directory in which to put predictions.
             plot_base_dir: The directory in which to put evaluation plots.
             eval_run_cfg: The `EvalRunConfig` for the evaluation run.
-            age_transform: The function that should be applied to columns that "represent" age
-                (e.g., prediction and uncertainty). Ensures comparison with "normal" dataset in the
-                same unit (year, month, etc.).
+            age_to_year_transform: The function that should be applied to columns that "represent"
+                age (e.g., prediction and uncertainty). Ensures comparison with "normal" dataset in
+                the same unit (year, month, etc.). It should transform the values to years.
         """
         ...
 
-    @abstractmethod
     def ood_preds_avail(self, eval_cfg_name: str) -> bool:
         """Check wether Predictions for OOD Eval are available, or not."""
-        ...
+        for ood_name in self.ood_datasets.keys():
+            pred_file = self._get_pred_filepath(eval_cfg_name, ood_name)
+            if not os.path.exists(pred_file):
+                return False  # If single pred file missing: return False
+        return True
 
     @abstractmethod
     def generate_predictions(
@@ -63,3 +81,167 @@ class OutOfDomainEvaluator(ABC):
     def generate_plots(self, eval_runs_data: dict[str, EvalRunData]) -> None:
         """Generate Out-of-Domain comparison plots for all eval runs from given `eval_runs_data`."""
         ...
+
+    def _get_pred_filepath(self, eval_cfg_name: str, ood_name: str) -> str:
+        """Generate Filepath for OoD Predictions (CSV) based on `eval_cfg_name` and `ood_name`."""
+        return os.path.join(self.data_base_dir, f"{eval_cfg_name}_{ood_name}_ood_preds.csv")
+
+    def _load_pred_file(self, eval_cfg_name: str, ood_name: str) -> pd.DataFrame | None:
+        """Load Prediction DataFrame and apply Age Transforms."""
+        ood_pred_file = self._get_pred_filepath(eval_cfg_name, ood_name)
+        if not os.path.isfile(ood_pred_file):
+            logger.warning(
+                "No OoD-Pred file for %s, %s (%s)", eval_cfg_name, ood_name, ood_pred_file
+            )
+            return None
+        ood_preds = pd.read_csv(ood_pred_file)
+        for col in ["prediction", "uncertainty"]:
+            ood_preds[col] = self.age_to_year_transform(ood_preds[col])
+        return ood_preds
+
+    def _get_fig(self):
+        return plt.subplots(figsize=(10, 7), dpi=250)
+
+    def _save_fig(self, fig: Figure, name: str) -> None:
+        os.makedirs(self.plot_dir, exist_ok=True)
+        fig.savefig(os.path.join(self.plot_dir, f"{name}.png"))
+
+    def _generate_uq_comparison_plot(
+        self,
+        eval_runs_data: dict[str, EvalRunData],
+        orig_data_label: str,
+    ) -> None:
+        """
+        Generate a plot comparing the Uncertainties of regular data with OoD datasets.
+
+        Comparison is grouped by `eval_cfg_name` from given `eval_runs_data`.
+
+        Args:
+            eval_runs_data: The runs on "normal" data that the OoD predictions should be compared
+                with. If no OoD data is available for any of the included eval runs (e.g., the
+                predictions have not been created for that UQ method, it is skipped)
+            orig_data_label: The name of the "regular" data (in the legend and title).
+        """
+        violin_positions = []
+        violin_labels = []
+        violin_datas = []
+        violin_colors = []
+        violin_hatches = []
+        violin_edge_color = []
+        legend_elements = [
+            Patch(facecolor="white", edgecolor="black", label=orig_data_label),
+        ]
+
+        v_pos = 0
+        for eval_cfg_name, eval_run_data in eval_runs_data.items():
+            v_pos += 1
+            uq_name = eval_run_data["display_name"]
+            uq_preds = eval_run_data["prediction_log"]
+            color = eval_run_data["color"]
+
+            avail_hatches = HATCHES.copy()
+
+            # Base Entry: UQ with "normal" dataset
+            violin_positions.append(v_pos)
+            violin_labels.append(uq_name)
+            violin_datas.append(uq_preds["uncertainty"].tolist())
+            violin_colors.append(color)
+            violin_hatches.append(None)  # no hatch for baseline dataset
+            violin_edge_color.append("black")
+
+            for ood_name, ood_cfg in self.ood_datasets.items():
+                ood_preds = self._load_pred_file(eval_cfg_name, ood_name)
+                if ood_preds is None:
+                    continue
+
+                v_pos += 1
+                violin_positions.append(v_pos)
+                violin_labels.append("")
+                violin_datas.append(ood_preds["uncertainty"].tolist())
+                violin_colors.append(color)
+                hatch = avail_hatches.pop(0)
+                violin_hatches.append(hatch)
+                violin_edge_color.append("white")
+
+                if len(legend_elements) < len(self.ood_datasets.items()) + 1:
+                    patch = Patch(
+                        facecolor="white", edgecolor="grey", hatch=hatch, label=ood_cfg["name"]
+                    )
+                    legend_elements.append(patch)
+            v_pos += 1
+
+        fig, ax = self._get_fig()
+        vplots = ax.violinplot(violin_datas, violin_positions, showmeans=True, widths=1)
+        v_patch: PolyCollection
+        for v_patch, color, hatch in zip(vplots["bodies"], violin_colors, violin_hatches):
+            v_patch.set_facecolor(color)
+            if hatch:
+                v_patch.set_hatch(hatch)
+                v_patch.set_edgecolor(color)
+        for partname in ("cbars", "cmins", "cmaxes", "cmeans"):
+            vplots[partname].set_edgecolor("black")
+            vplots[partname].set_linewidth(1)
+        ax.set_xticks(violin_positions)
+        ax.set_xticklabels(violin_labels, rotation=15)
+        ax.set_ylabel("Uncertainty")
+        ax.legend(handles=legend_elements, handleheight=3, handlelength=4, loc="upper left")
+        fig.suptitle(f"Out-of-Domain Data Uncertainty Comparison - {orig_data_label}")
+        self._save_fig(fig, "uq_comparison")
+
+    def _generate_prediction_comparison_plot(
+        self,
+        eval_runs_data: dict[str, EvalRunData],
+    ) -> None:
+        """Violin-Plot Comparison of the Predictions for the OOD Datasets by the UQ-Models."""
+        violin_positions = []
+        violin_labels = []
+        violin_datas = []
+        violin_colors = []
+        violin_hatches = []
+        violin_edge_color = []
+        legend_elements = []
+
+        v_pos = 0
+        for eval_cfg_name, eval_run_data in eval_runs_data.items():
+            uq_name = eval_run_data["display_name"]
+            color = eval_run_data["color"]
+
+            avail_hatches = HATCHES.copy()
+
+            for ood_name, ood_cfg in self.ood_datasets.items():
+                ood_preds = self._load_pred_file(eval_cfg_name, ood_name)
+                if ood_preds is None:
+                    continue
+
+                v_pos += 1
+                violin_positions.append(v_pos)
+                violin_labels.append(uq_name)
+                violin_datas.append(ood_preds["prediction"].tolist())
+                violin_colors.append(color)
+                hatch = avail_hatches.pop(0)
+                violin_hatches.append(hatch)
+                violin_edge_color.append("white")
+
+                if len(legend_elements) < len(self.ood_datasets.items()):
+                    patch = Patch(
+                        facecolor="white", edgecolor="grey", hatch=hatch, label=ood_cfg["name"]
+                    )
+                    legend_elements.append(patch)
+            v_pos += 1
+
+        fig, ax = self._get_fig()
+        vplots = ax.violinplot(violin_datas, violin_positions, showmeans=True, widths=1)
+        v_patch: PolyCollection
+        for v_patch, color, hatch in zip(vplots["bodies"], violin_colors, violin_hatches):
+            v_patch.set_facecolor(color)
+            if hatch:
+                v_patch.set_hatch(hatch)
+                v_patch.set_edgecolor(color)
+        for partname in ("cbars", "cmins", "cmaxes", "cmeans"):
+            vplots[partname].set_edgecolor("black")
+            vplots[partname].set_linewidth(1)
+        ax.set_xticks(violin_positions)
+        ax.set_xticklabels(violin_labels, rotation=15)
+        ax.set_ylabel("Prediction (Age in Years)")
+        ax.legend(handles=legend_elements, handleheight=3, handlelength=4, loc="upper right")
+        self._save_fig(fig, "prediction_comparison")
